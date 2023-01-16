5a5d582d8987860419a5c47b13fe99f18e19aa10
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-01-15 19:21:47 -0500

    s09-78 Modules and Packages

---
+++ s09-modules-and-packages/MyMainPackage/SubPackage/my_sub_script.py
@@ -0,0 +1,2 @@
+def sub_report():
+    print("Hey, I am a function inside my_sub_script.py")
---
+++ s09-modules-and-packages/myprogram.py
@@ -0,0 +1,9 @@
+from mymodule import my_func
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import my_sub_script
+
+my_func()
+
+some_main_script.report_main()
+
+my_sub_script.sub_report()
