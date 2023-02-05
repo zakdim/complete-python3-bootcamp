c97a6bb7c173f18fa8739afcbdf3247111957d7f
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-02-04 19:25:56 -0500

    s11-97 Solution Walkthrough - Final Gameplay Script

--- s11-milestone-project-2/blackjack-game.py
+++ s11-milestone-project-2/blackjack-game.py
@@ -7,11 +7,13 @@ def take_bet(chips: bj.Chips):
     while True:
         try:
             chips.bet = int(input("How many chips would you like to bet? "))
-        except:
+        except ValueError:
             print("Sorry, please provide an integer")
         else:
             if chips.bet > chips.total:
                 print("Sorry, you do not have enough chips! You have: {}".format(chips.total))
+            else:
+                break
 
 
 def hit(deck: bj.Deck, hand: bj.Hand):
@@ -23,15 +25,19 @@ def hit_or_stand(deck: bj.Deck, hand: bj.Hand):
     global playing  # to control an upcoming while loop
 
     while True:
-        x = input("Hit or Stand? Enter h or s ")
+        x = input("Hit or Stand? Enter h/s: ")
 
         if x[0].lower() == 'h':
             hit(deck, hand)
+
         elif x[0].lower() == 's':
             print("Player stands. Dealer is playing.")
             playing = False
+
         else:
             print("Sorry, please try again")
+            continue
+        break
 
 
 def show_cards(cards):
@@ -68,7 +74,7 @@ def show_all(player: bj.Hand, dealer: bj.Hand):
 
 
 def player_busts(player: bj.Hand, dealer: bj.Hand, chips: bj.Chips):
-    print('BUST PLAYER!')
+    print('PLAYER BUSTS!')
     chips.lose_bet()
 
 
@@ -78,7 +84,7 @@ def player_wins(player: bj.Hand, dealer: bj.Hand, chips: bj.Chips):
 
 
 def dealer_busts(player: bj.Hand, dealer: bj.Hand, chips: bj.Chips):
-    print('PLAYER WINS! DEALER BUSTED!')
+    print('DEALER BUSTS!')
     chips.win_bet()
 
 
@@ -87,31 +93,100 @@ def dealer_wins(player: bj.Hand, dealer: bj.Hand, chips: bj.Chips):
     chips.lose_bet()
 
 
-def push():
-    print('Dealer and player tie! PUSH')
+def push(player: bj.Hand, dealer: bj.Hand):
+    print("Dealer and player tie! It's a PUSH")
 
 
-test_deck = bj.Deck()
-print(f'Deck size: {len(test_deck.deck)}')
-test_deck.shuffle()
-# print(f'{test_deck}')
+def play_game():
+    global playing
+    hand_num = 0
+    player_chips = bj.Chips()
+    while True:
+        hand_num += 1
+        # Print an opening statement
+        print("WELCOME TO BLACKJACK hand: {}".format(hand_num))
 
-# PLAYER
-test_player = bj.Hand()
-print(f'{test_player}')
+        # Create & shuffle the deck, dela two cards to each player
+        deck = bj.Deck()
+        deck.shuffle()
 
-# Deal 1 card from the deck Card(suit, rank)
-test_player.add_card(test_deck.deal())
-print(f'{test_player}')
-print(f'Deck size: {len(test_deck.deck)}')
+        player_hand = bj.Hand()
+        player_hand.add_card(deck.deal())
+        player_hand.add_card(deck.deal())
 
-test_player.add_card(test_deck.deal())
-print(f'{test_player}')
-print(f'Deck size: {len(test_deck.deck)}')
+        dealer_hand = bj.Hand()
+        dealer_hand.add_card(deck.deal())
+        dealer_hand.add_card(deck.deal())
 
-dealer = bj.Hand()
-dealer.add_card(test_deck.deal())
-dealer.add_card(test_deck.deal())
+        # Set up the Player's chips
+        if hand_num > 1:
+            player_chips.bet = 0    # On subsequent hand rounds only rest bet
+        print(f"Player's chips: {player_chips}")
 
-show_some(test_player, dealer)
-show_all(test_player, dealer)
+        # Prompt the Player for their bet
+        take_bet(player_chips)
+
+        # Show cards (but keep one dealer card hidden)
+        show_some(player_hand, dealer_hand)
+
+        while playing:  # recall this variable from our hit_or_stand function
+
+            # Prompt for Player to Hit or Stand
+            hit_or_stand(deck, player_hand)
+
+            # Show cards (but keep one dealer card hidden)
+            show_some(player_hand, dealer_hand)
+
+            # If player's hand exceeds 21, run player_busts() and break out of loop
+            if player_hand.value > 21:
+                player_busts(player_hand, dealer_hand, player_chips)
+                break
+
+        # If Player hasn't busted, play Dealer's hand until Dealer reaches 17
+        if player_hand.value <= 21:
+            while dealer_hand.value < 17:  # or < player_hand.value
+                hit(deck, dealer_hand)
+
+            # Show all cards
+            show_all(player_hand, dealer_hand)
+
+            # Run different winning scenarios
+            if dealer_hand.value > 21:
+                dealer_busts(player_hand, dealer_hand, player_chips)
+
+            elif dealer_hand.value > player_hand.value:
+                dealer_wins(player_hand, dealer_hand, player_chips)
+
+            elif dealer_hand.value < player_hand.value:
+                player_wins(player_hand, dealer_hand, player_chips)
+
+            else:
+                push(player_hand, dealer_hand)
+
+        # Inform Player of their chips total
+        print("\nPlayer's total chips are at: {}".format(player_chips.total))
+
+        # Ask to play again
+        if player_chips.total > 0:
+            new_game = input("Would you like to play another hand? y/n: ")
+            if new_game[0].lower() == 'y':
+                playing = True
+                continue
+            else:
+                print('Thank you for playing!')
+                break
+        else:
+            reset_game = input("You are out of chips, would you like to reset and play another hand? y/n: ")
+            if reset_game[0].lower() == 'y':
+                player_chips = bj.Chips()
+                playing = True
+                continue
+            else:
+                print('Thank you for playing!')
+                break
+
+
+if __name__ == "__main__":
+    play_game()
+else:
+    print('blackjack-game.py has been imported')
