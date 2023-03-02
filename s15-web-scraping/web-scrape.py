123508265862438604e6b52ad786738b73a35ad8
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-03-01 23:33:36 -0500

    s14-119 Python Web Scraping - Grabbing a Class

--- s15-web-scraping/web-scrape.py
+++ s15-web-scraping/web-scrape.py
@@ -1,27 +1,63 @@
 import bs4
 import requests
 
-url: str = 'https://example.com/'
 
-result = requests.get(url)
-# print(type(result))
-# print(f'result.text:\n{"="*30}\n{result.text}')
+def print_value(label, value):
+    print(f'{label}:\n{"=" * 30}\n{value}')
 
-soup = bs4.BeautifulSoup(result.text, 'html.parser')
-# print(f'soup:\n{"="*30}\n{soup}')
 
-title = soup.select('title')
-print(f'title:\n{"="*30}\n{title}')
+def s15_118():
+    url: str = 'https://example.com/'
 
-paragraphs = soup.select('p')
-print(f'paragraphs:\n{"="*30}\n{paragraphs}')
+    result = requests.get(url)
+    # print(type(result))
+    # print(f'result.text:\n{"="*30}\n{result.text}')
 
-h1 = soup.select('h1')
-print(f'h1:\n{"="*30}\n{h1}')
+    soup = bs4.BeautifulSoup(result.text, 'html.parser')
+    print_value('soup', soup)
 
-# Grab text only
-title = soup.select('title')[0].getText()
-print(f'title:\n{"="*30}\n{title}')
+    title = soup.select('title')
+    print_value('title', title)
 
-paragraphs = soup.select('p')
-print(f'paragraphs:\n{"="*30}\n{paragraphs[0].getText()}')
+    paragraphs = soup.select('p')
+    print_value('paragraphs', paragraphs)
+
+    h1 = soup.select('h1')
+    print_value('h1', h1)
+
+    # Grab text only
+    title = soup.select('title')[0].getText()
+    print_value('title', title)
+
+    paragraphs = soup.select('p')
+    print_value('paragraphs', paragraphs)
+
+
+# s15-119: Grabbing a class
+def s15_119():
+    res = requests.get('https://en.wikipedia.org/wiki/Grace_Hopper')
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    # print_value('soup', soup)
+
+    # search for .vector-toc-text
+    toc_div = soup.select('div.vector-toc-text')
+    # print_value('toc_text', toc_div)
+
+    for div in toc_div:
+        toc_num_span = div.select('span.vector-toc-numb')
+        toc_num = ''
+        toc_text = ''
+        if len(toc_num_span) > 0:
+            toc_num = toc_num_span[0].text
+
+        if len(div) >= 3:
+            toc_text = div.contents[2]
+        elif len(div) >=1:
+            toc_text = div.contents[0]
+
+        print(f'{toc_num}{" " if bool(toc_num) else ""}{toc_text}')
+
+
+if __name__ == '__main__':
+    # s15_118()
+    s15_119()
