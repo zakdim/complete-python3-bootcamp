583d145f13ecf75713173a7083dd1b146b93a8b3
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-03-25 17:27:31 -0400

    s20-141 Advanced Dictionaries

---
+++ s20-advanced-objects-and-data-structures/s20-141-advanced-dict.py
@@ -0,0 +1,23 @@
+def s20_141():
+    d = {'k1': 1, 'k2': 2}
+
+    d2 = {x: x**2 for x in range(10)}
+    print(d2)
+
+    d3 = {k: v**2 for k, v in zip(['a', 'b'], range(2))}
+    print(d3)
+
+    for k in d.items():
+        print(k)
+    for k, v in d.items():
+        print(k, v)
+    for v in d.values():
+        print(v)
+    for k in d.keys():
+        print(k)
+
+    
+
+
+if __name__ == '__main__':
+    s20_141()
